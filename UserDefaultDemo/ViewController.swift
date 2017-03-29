647173d534ae4f7cfaa0207d07c60c1a32d0be7d
PhongLe <phongle@phongles-macbook-pro.local>
2017-03-29 09:39:54 +0700

    Initial Commit

---
+++ UserDefaultDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UserDefaultDemo
+//
+//  Created by PhongLe on 3/29/17.
+//  Copyright © 2017 PhongLe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
